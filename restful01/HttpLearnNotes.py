f7f1f6e9c86e37ba65306a290efe4600537ee4f4
toby <toby.fangyuan@gmail.com>
2021-01-12 02:28:39 -0800

    add Httpie POST example.

--- restful01/HttpLearnNotes.py
+++ restful01/HttpLearnNotes.py
@@ -9,4 +9,11 @@ $ curl -iX GET localhost:8000/toys/
 
 if you have Httpie package installed, even better print:
 $ http :8000/toys/
+
+now we check detailed items
+$ http :8000/toys/3  
+
+now we post to add new toy
+$  http POST :8000/toys/ name="PvZ 2 puzzle" description="plants vs zombies" \
+    toy_category="Puzzle" was_included_in_home=false release_date="2017-10-08T01:01:00.777777Z"
 """
